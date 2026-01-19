6a1b0e088a81e844c1eaf630935da5a6ea02ede2
kirtansarvadhi <kirtan.sarvadhi@gmail.com>
2026-01-19 18:44:08 +0530

    updataed

--- jewelry-app/src/server.js
+++ jewelry-app/src/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
